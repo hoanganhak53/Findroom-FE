800151ca3109832aad7e581573aa6ff04e358645
Hoanganh <hoanganhak53@gmail.com>
2023-02-22 21:23:17 +0700

    bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-    HashRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Login from './views/Login';
